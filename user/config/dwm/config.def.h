b7c4e792cf0742ace117108099268915acdbc0da
vyanix <186918137+vyanix@users.noreply.github.com>
2026-02-16 22:45:14 +0530

    chore: assign float to qalculate window

--- user/config/dwm/config.def.h
+++ user/config/dwm/config.def.h
@@ -45,10 +45,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */	
-	{ "kitty",        NULL,   NULL,       1 << 0,       0,           -1 },
+	/* class               instance    title       tags mask     isfloating   monitor */	
+	{ "kitty",                 NULL,   NULL,       1 << 0,       0,           -1 },
 	{ ".virt-manager-wrapped", NULL,   NULL,       1 << 1,       0,           -1 },
-	{ "pavucontrol",  NULL,   NULL,       0,            1,           -1 },
+	{ "pavucontrol",           NULL,   NULL,       0,            1,           -1 },
+	{ "qalculate-gtk",         NULL,   NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
